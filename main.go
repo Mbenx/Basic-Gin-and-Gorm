cc7861e80d998b1dbcef0373aee3a6c641383b4a
Mbenx <herlambang.srihartono@gmail.com>
2019-10-28 14:38:59 +0700

    Part-1

--- main.go
+++ main.go
@@ -3,11 +3,35 @@ package main
 import "github.com/gin-gonic/gin"
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
+	router := gin.Default()
+	router.GET("/", getHome)
+	router.GET("/article/:title", getArticle)
+	router.POST("/article", postArticle)
+
+	router.Run() // listen and serve on 0.0.0.0:8080
+}
+
+func getHome(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "Welcome to Golang",
 	})
-	r.Run() // listen and serve on 0.0.0.0:8080
+}
+
+func getArticle(c *gin.Context) {
+	title := c.Param("title")
+	c.JSON(200, gin.H{
+		"message": "Welcome to Golang",
+		"title":   title,
+	})
+}
+
+func postArticle(c *gin.Context) {
+	title := c.PostForm("title")
+	desc := c.PostForm("desc")
+
+	c.JSON(200, gin.H{
+		"message": "Post Berhasil",
+		"title":   title,
+		"desc":    desc,
+	})
 }
